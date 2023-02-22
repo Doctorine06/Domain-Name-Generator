0aa5889c2c5d83c466b64a45c3dcd408f094fb0f
Doctorine06 <fergtheartist@gmail.com>
2023-02-22 00:11:55 +0000

    turn in

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  document.querySelector("#excuse").innerHTML = ExcuseGen();
   console.log("Hello Rigo from the console!");
+};
+
+let ExcuseGen = () => {
+  let pronoun = ["I", "The"];
+  let adjective = ["giant", "delicious", "gorgeous"];
+  let noun = ["bug", "music"];
+
+  let pronounindex = Math.floor(Math.random() * pronoun.length);
+  let adjectiveindex = Math.floor(Math.random() * adjective.length);
+  let nounindex = Math.floor(Math.random() * noun.length);
+
+  return (
+    pronoun[pronounindex] + adjective[adjectiveindex] + noun[nounindex] + ".com"
+  );
 };
